e9604672479c0589086b5b8bac243ada15e6aff9
jasonjpenfold <285502957+jasonjpenfold@users.noreply.github.com>
2026-09-21 20:06:31 +0100

    UI altered to show state

--- ContentView.swift
+++ ContentView.swift
@@ -4,21 +4,49 @@ struct ContentView: View {
     @State private var torchIsOn = false
     @Environment(TorchViewModel.self) private var model
     var body: some View {
-        
-        VStack {
-            Image(systemName: "flashlight.on.circle.fill")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Torch!")
+        ZStack{
+            Color.black.opacity(model.isOn ? 0.9 : 0.5)
             
-            Button(model.isOn ? "On":"Off"){
-                model.toggleTorchMode()
+            VStack(spacing: 36) {
                 
-            }.buttonStyle(.glassProminent)
-            
+                Spacer()
+                
+                Text(model.isOn ? "TORCH ON" : "TORCH OFF")
+                        .font(.system(.subheadline, design: .rounded))
+                        .bold()
+                        .foregroundStyle(model.isOn ? .white : .black)
+                        .shadow(color: model.isOn ? .yellow : .clear, radius: 40)
+                        
+                Spacer()
+                Image(systemName: "flashlight.on.fill")
+                    .font(.system(size: 120, weight: .bold, design: .rounded))
+                    .foregroundStyle(model.isOn ? .yellow : .white)
+                    .scaleEffect(model.isOn ? 1.05 : 1)
+                    
+                Spacer()
+                Button{
+                    withAnimation{
+                        model.toggleTorchMode()
+                    }
+                                        
+                }label:{
+                    Image(systemName: "power.circle").foregroundStyle(.white)
+                        .font(.system(size: 80, weight: .medium, design: .rounded))
+                    
+                    
+                }
+                .buttonStyle(.borderless)
+                .tint(model.isOn ? .orange : .gray)
+                
+                Spacer()
+                
+                
+            }.padding()
             
         }
     }
+        
+    
     
     
 }
